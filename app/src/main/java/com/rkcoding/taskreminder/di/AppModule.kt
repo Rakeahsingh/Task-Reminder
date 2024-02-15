9a6c7943073d4505a5b46773973c837ca5c6db58
Rakeahsingh <bca02121502019@gmail.com>
2024-02-15 20:18:08 +0530

    Initial commit

---
+++ app/src/main/java/com/rkcoding/taskreminder/di/AppModule.kt
@@ -0,0 +1,20 @@
+package com.rkcoding.taskreminder.di
+
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth() = Firebase.auth
+
+}
